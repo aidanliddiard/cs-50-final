adba11a38585ec2f098c5449979ab8c1fe22f880
Aidan Liddiard <Aidan.Liddiard@cvent.com>
2024-03-27 15:20:58 -0700

    Merge branch 'concerts' of https://github.com/aidanliddiard/cs-50-final into dev

--- cs-50-final/src/App.tsx
+++ cs-50-final/src/App.tsx
@@ -18,22 +18,6 @@ function App() {
           <Route path="/artist/:id/:name" element={<Events />} />
           <Route path="/" element={<Artists />} />
         </Routes>
-        {/* {
-        fetchStateEvents === FetchState.DEFAULT && <button onClick={getEvents}>Get Events</button>
-      }
-      {fetchStateEvents === FetchState.LOADING && <p>Loading...</p>}
-      {fetchStateEvents === FetchState.ERROR && <p>There was an error</p>}
-      {fetchStateEvents === FetchState.SUCCESS && (
-        <ul>
-        {events.map((event) => (
-          <li key={event.id}>
-          <h2>{event.name}</h2>
-          <p>{event.dates.start.localDate} at {event.dates.start.localTime}</p>
-          <p>{event.url}</p>
-          </li>
-          ))}
-          </ul>
-        )} */}
       </BrowserRouter>
     </>
   );
